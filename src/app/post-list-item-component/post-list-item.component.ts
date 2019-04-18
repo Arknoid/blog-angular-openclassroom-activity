f3f158f3cad1fc53cf9052b3c69fadc39164effc
OlivierM <omalige@gmail.com>
2019-04-18 10:17:22 +0200

    finish

--- src/app/post-list-item-component/post-list-item.component.ts
+++ src/app/post-list-item-component/post-list-item.component.ts
@@ -8,9 +8,38 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PostListItemComponent implements OnInit {
   @Input() post: Post;
-  constructor() { }
+  likeCounter: number;
+  liked: boolean;
+  equalLike: boolean;
 
+  constructor() {
+    this.likeCounter = 0;
+    this.liked = false;
+    this.equalLike = true;
+  }
   ngOnInit() {
   }
 
+  addLike() {
+    this.likeCounter ++;
+    this.checkLiked();
+  }
+
+  addUnlike() {
+    this.likeCounter --;
+    this.checkLiked();
+  }
+
+  checkLiked() {
+    if (this.likeCounter > 0) {
+      this.liked = true;
+      this.equalLike = false;
+    } else if (this.likeCounter === 0) {
+      this.equalLike = true;
+    } else {
+      this.liked = false;
+      this.equalLike = false;
+    }
+
+  }
 }
